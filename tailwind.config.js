3296b47c596b54281cba9807ab968396ebd66640
BoburBerdiev <sirofim012@gmail.com>
2024-05-06 16:50:44 +0500

    bismillah

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+        lg: '2rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      },
+    },
+    extend: {
+      fontFamily: {
+        jost: ["var(--jost)"],
+        forum: ["var(--font-forum)"],
+      },
+      colors: {
+       currentBlue:'#00274D',
+        currentBlack:'#444444'
+      },
+      boxShadow: {
+        'inner': '[-1px_3px_5px_0px_rgba(0,0,0,0.75)]',
+      }
+    },
+  },
+  plugins: [],
+};
